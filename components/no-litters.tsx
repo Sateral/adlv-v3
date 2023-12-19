e72dacc67108f41d99326d951da7826e4d5b8b83
Satty <kopylenkod@gmail.com>
2023-12-19 15:53:49 -0500

    fixed ESLINT

--- components/no-litters.tsx
+++ components/no-litters.tsx
@@ -7,7 +7,7 @@ const NoLitters = () => {
       <p className="font-jost text-4xl text-primary-tan font-medium">Sorry!</p>
       <Image src={SadCat} alt="Frowning Cat" className="max-w-[150px]" />
       <p className="font-jost sm:text-4xl text-2xl text-primary-tan font-medium text-center">
-        As of right now, we have no kittens.
+        As of right now&apos; we have no kittens.
       </p>
       <p className="md:w-[700px] sm:w-[500px] sm:text-xl text-lg font-jost mt-4 text-center">
         If you're interested in making one a part of your family, contact us to
